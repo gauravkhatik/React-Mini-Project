651087e18c21edcf78f74d12e40e3e44b5ae6083
gauravkhatik <gauravkkhatik07@gmail.com>
2025-07-27 21:00:00 +0530

    1 project deleted from git repo

--- Bharatclock/src/assets/components/Bpara.jsx
+++
@@ -1,9 +0,0 @@
-
-const Bpara = () => {
-  return (
-   <p>This is the clock that shows the time in Bharat at all times</p>
-  
-  )
-}
-
-export default Bpara
--- Bharatclock/src/assets/components/Ctime.jsx
+++
@@ -1,9 +0,0 @@
-const Ctime = () => {
-  let time = new Date()
-return <p>
-  This is current time: {time.toLocaleTimeString()} - {time.toLocaleDateString()}
-</p>
- 
-}
-
-export default Ctime
